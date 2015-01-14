0ee656eb26747ed7b38a4e92d691e849c5dd023a
Ben Manning <bmanning@me.com>
2015-01-14 11:44:50 -0800

    add files

---
+++ db/migrate/20150114032240_add_magazine_id_to_subscription.rb
@@ -0,0 +1,4 @@
+class AddMagazineIdToSubscription < ActiveRecord::Migration
+  def change
+  end
+end
---
+++ db/migrate/20150114032331_add_user_id_to_subscription.rb
@@ -0,0 +1,4 @@
+class AddUserIdToSubscription < ActiveRecord::Migration
+  def change
+  end
+end
